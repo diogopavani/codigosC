47aa2a2914bbc234221441f51d50141260e395b0
diogopavanu1 <55139291+diogopavanu1@users.noreply.github.com>
2019-09-30 21:28:26 -0300

    Update lista3.c

--- lista3.c
+++ lista3.c
@@ -1,1 +1,22 @@
 
+//exercicio 1, lista 3:
+#include <stdio.h>
+#include <math.h>
+
+
+int main()
+{
+    int i=0;
+    int cont=0;
+    char nome[100];
+    printf("digite um char de ate 100caracteres");
+    gets(nome);
+    for(i=0; nome[i]!='\0';i++){
+            cont++;
+    }
+    printf("%d", cont);
+}
+
+//////////////////////FIM DO EX1//////////////////////////////////////
+
+
